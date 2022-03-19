f19e9cb9f2135babceadb059cadcd8bb92024401
Roberto <ruidaing@hotmail.com>
2022-03-19 09:55:26 -0500

    conversion a una funcion arroy
    una arroy function si solo tiene una linea no se necesita llaves y no utiliza return

--- src/MoviesGrid.jsx
+++ src/MoviesGrid.jsx
@@ -4,9 +4,9 @@ export function MoviesGrid() {
   //creamos una funcion javascript para recorrer el json
   return (
     <ul>
-      {movies.map(function (movie) {
-        return <li>{movie.title}</li>;
-      })}
+      {movies.map((movie) => (
+        <li>{movie.title}</li>
+      ))}
     </ul>
   );
 }
