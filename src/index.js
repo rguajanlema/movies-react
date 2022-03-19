29747047d3b010cd0d6a5fef9f1ab7e4b45836eb
Roberto <ruidaing@hotmail.com>
2022-03-19 08:35:40 -0500

    paso de parametros

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+function Contenido() {
+  return (
+    <div>
+      <h1>Titulo</h1>
+      <div>Hola desde React</div>
+    </div>
+  );
+}
+
+ReactDOM.render(
+  <Contenido titulo="Titulo" contenido="Hola desde React" />,
+  document.getElementById("root")
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
