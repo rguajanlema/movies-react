418dc6c7130e9d4b0bc2b0756164637028c536ab
Roberto <ruidaing@hotmail.com>
2022-03-19 10:31:12 -0500

    mejora de presentacion de images

--- src/MovieCard.jsx
+++ src/MovieCard.jsx
@@ -4,7 +4,13 @@ export function MovieCard({ movie }) {
   const imageUrl = "https://image.tmdb.org/t/p/w300" + movie.poster_path;
   return (
     <li className={styles.movieCard}>
-      <img className={styles.movieImage} src={imageUrl} alt={movie.title} />
+      <img
+        width={230}
+        height={345}
+        className={styles.movieImage}
+        src={imageUrl}
+        alt={movie.title}
+      />
       <div>{movie.title}</div>
     </li>
   );
--- src/MoviesGrid.jsx
+++ src/MoviesGrid.jsx
@@ -1,11 +1,12 @@
 import { MovieCard } from "./MovieCard";
 import movies from "./movies.json";
+import styles from "./MoviesGrid.module.css";
 
 export function MoviesGrid() {
   //creamos una funcion javascript para recorrer el json
   //pasa el atributo movie
   return (
-    <ul>
+    <ul className={styles.moviesGrid}>
       {movies.map((movie) => (
         <MovieCard key={movie.id} movie={movie} />
       ))}
