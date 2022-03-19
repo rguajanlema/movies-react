6463c14298990153e1487da092ba4519c1c609ae
Roberto <ruidaing@hotmail.com>
2022-03-19 10:21:06 -0500

    agregado de estilos bordeados a las imagenes

--- src/MovieCard.jsx
+++ src/MovieCard.jsx
@@ -4,7 +4,7 @@ export function MovieCard({ movie }) {
   const imageUrl = "https://image.tmdb.org/t/p/w300" + movie.poster_path;
   return (
     <li className={styles.movieCard}>
-      <img src={imageUrl} alt={movie.title} />
+      <img className={styles.movieImage} src={imageUrl} alt={movie.title} />
       <div>{movie.title}</div>
     </li>
   );
