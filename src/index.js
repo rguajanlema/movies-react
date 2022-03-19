1a83abf55e747ef779274c036bd0248c08955e5c
Roberto <ruidaing@hotmail.com>
2022-03-19 09:39:36 -0500

    creacion e importacion de un nuevo componente

--- src/index.js
+++ src/index.js
@@ -1,20 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-
-//props el parametro de entrada
-//otra de obtener el contenido
-function Contenido({ titulo, children }) {
-  return (
-    <div>
-      <h1>{titulo}</h1>
-      <div>{children}</div>
-    </div>
-  );
-}
+import { App } from "./App";
 
 ReactDOM.render(
   //pasamos contenidos children como si fuera una variable
-  <Contenido titulo="Titulo 1">Hola desde React</Contenido>,
+  <App />,
   document.getElementById("root")
 );
