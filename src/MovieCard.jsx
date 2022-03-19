780d6d117e378f002b0691bdd052888002284e69
Roberto <ruidaing@hotmail.com>
2022-03-19 10:09:50 -0500

    Impresion de imagen
    creamos la url de imagen y realizamos la visualizacion de la imagen

--- src/MovieCard.jsx
+++ src/MovieCard.jsx
@@ -1,3 +1,9 @@
 export function MovieCard({ movie }) {
-  return <li>{movie.title}</li>;
+  const imageUrl = "https://image.tmdb.org/t/p/w300" + movie.poster_path;
+  return (
+    <li>
+      <img src={imageUrl} alt={movie.title} />
+      {movie.title}
+    </li>
+  );
 }
