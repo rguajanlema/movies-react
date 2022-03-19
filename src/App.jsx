ff9dbea9ea0f876aca33649ef9c367d7d461e50e
Roberto <ruidaing@hotmail.com>
2022-03-19 11:48:14 -0500

    actualizar la version de react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,19 @@
-import { MoviesGrid } from "./components/MoviesGrid";
-import styles from "./App.module.css";
+import React from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+
+//import { MoviesGrid } from "./components/MoviesGrid";
+//import styles from "./App.module.css";
 
 export function App() {
   return (
-    <div>
-      <header>
-        <h1 className={styles.title}>Movies</h1>
-      </header>
+    <Router>
+      <header>{/*<h1 className={styles.title}>Movies</h1>*/}</header>
       <main>
-        <MoviesGrid />
+        <Switch>
+          <Route path="/movie">Movies</Route>
+          <Route path="/">Home</Route>
+        </Switch>
       </main>
-    </div>
+    </Router>
   );
 }
